e4ce253e5c1bee2c52f1e7aa1a1d28436bda3f55
binhsonnguyen <son@binhsonnguyen.com>
2018-02-05 10:33:31 +0700

    first degree equation computer

--- src/FirstDegreeEquation.java
+++ src/FirstDegreeEquation.java
@@ -1,3 +1,30 @@
-public class FirstDegreeEquation {
+import java.util.Scanner;
 
+public class FirstDegreeEquation {
+    public static void main(String[] args) {
+
+        System.out.println("First Degree Equation Computer");
+        System.out.println("Given a equation as 'a*x + b = c', please enter arguments:");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("\ta: ");
+        double a = scanner.nextDouble();
+
+        if (a == 0) {
+            System.out.print("With a = 0, equation pass with any 'x'");
+        } else {
+
+            System.out.print("\tb: ");
+            double b = scanner.nextDouble();
+
+            System.out.print("\tc: ");
+            double c = scanner.nextDouble();
+
+            double answer = (c - b) / a;
+            System.out.printf("Equation pass with x = %f\n", answer);
+        }
+
+        System.out.println("Thanks you! Goodbye!");
+
+    }
 }
